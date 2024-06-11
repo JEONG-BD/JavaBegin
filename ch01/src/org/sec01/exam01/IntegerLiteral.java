2dd39628e0d3dc1f0697f0589f2bb5c6026ce29d
JEONG-BD <dev.byeongdu@gmail.com>
2024-06-12 08:29:52 +0900

    Integer, Long, Byte

---
+++ ch01/src/org/sec01/exam01/IntegerLiteral.java
@@ -0,0 +1,15 @@
+package org.sec01.exam01;
+
+public class IntegerLiteral {
+    public static void main(String[] args){
+        int var1 = 0b1011; //Binary
+        int var2 = 0206;   //octal
+        int var3 = 365;    //decimal
+        int var4 = 0xB3;   //decimal
+
+        System.out.println("var 1 :" + var1);
+        System.out.println("var 2 :" + var2);
+        System.out.println("var 3 :" + var3);
+        System.out.println("var 4 :" + var4);
+    }
+}
